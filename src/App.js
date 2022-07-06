608de9e988fe74c1e333e3eff5fd5495249ae47e
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-07-06 14:42:51 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import BlogsPage from "./Pages/Blogs";
+import { useState, useEffect } from "react";
+
+const urlEndpoint = "http://localhost:4000";
+
+function App() {
+  const [serverJSON, setServerJSON] = useState({ message: null });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiResponse = await fetch(`${urlEndpoint}/blogs/hello-blogs`);
+      const apiJSON = await apiResponse.json();
+      setServerJSON(apiJSON);
+      return;
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          path="/blogs"
+          element={<BlogsPage message={serverJSON.message} />}
+        ></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Blogs.js
@@ -0,0 +1,10 @@
+const BlogsPage = (props) => {
+  return (
+    <div className="blogs-page">
+      <h1>Blogs Page</h1>
+      <p>Server Message: {props.message}</p>
+    </div>
+  );
+};
+
+export default BlogsPage;
