3d5cf846d33e550f9d1cf3ec8207cb9dc3b0d035
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-07-06 16:54:24 -0500

    assigment day 2 complete without stretch goal for styling

--- src/App.js
+++ src/App.js
@@ -6,11 +6,22 @@ import { useState, useEffect } from "react";
 const urlEndpoint = "http://localhost:4000";
 
 function App() {
-  const [serverJSON, setServerJSON] = useState({ message: null });
+  const [serverJSON, setServerJSON] = useState(null);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const apiResponse = await fetch(`${urlEndpoint}/blogs/hello-blogs`);
+  //     const apiJSON = await apiResponse.json();
+  //     setServerJSON(apiJSON);
+  //     return;
+  //   };
+  //   fetchData();
+  // }, []);
 
   useEffect(() => {
     const fetchData = async () => {
-      const apiResponse = await fetch(`${urlEndpoint}/blogs/hello-blogs`);
+      const url = `${urlEndpoint}/blogs/all-blogs`;
+      const apiResponse = await fetch(url);
       const apiJSON = await apiResponse.json();
       setServerJSON(apiJSON);
       return;
@@ -21,10 +32,12 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route
-          path="/blogs"
-          element={<BlogsPage message={serverJSON.message} />}
-        ></Route>
+        {serverJSON && (
+          <Route
+            path="/blogs"
+            element={<BlogsPage blogs={serverJSON} />}
+          ></Route>
+        )}
       </Routes>
     </div>
   );
