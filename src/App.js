d40f4e880319c6e5beb74ed6c26ca77578053f52
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-07-07 17:05:01 -0500

    thursday end of day with stretchies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import BlogsPage from "./Pages/Blogs";
 import { useState, useEffect } from "react";
 
@@ -7,6 +7,12 @@ const urlEndpoint = "http://localhost:4000";
 
 function App() {
   const [serverJSON, setServerJSON] = useState({ message: [] });
+  const [sortField, setSortField] = useState("title");
+  const [sortOrder, setSortOrder] = useState("ASC");
+  const [filterField, setFilterField] = useState("title");
+  const [filterValue, setFilterValue] = useState("");
+  const [limit, setLimit] = useState(Number());
+  const [page, setPage] = useState(Number(1));
 
   // useEffect(() => {
   //   const fetchData = async () => {
@@ -18,23 +24,50 @@ function App() {
   //   fetchData();
   // }, []);
 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const url = `${urlEndpoint}/blogs/all-blogs`;
+  //     const apiResponse = await fetch(url);
+  //     const apiJSON = await apiResponse.json();
+  //     setServerJSON(apiJSON);
+  //     return;
+  //   };
+  //   fetchData();
+  // }, []);
+
   useEffect(() => {
     const fetchData = async () => {
-      const url = `${urlEndpoint}/blogs/all-blogs`;
+      const url = `${urlEndpoint}/blogs/all-blogs?sortField=${sortField}&sortOrder=${sortOrder}&filterField=${filterField}&filterValue=${filterValue}&limit=${limit}&page=${page}`;
       const apiResponse = await fetch(url);
       const apiJSON = await apiResponse.json();
       setServerJSON(apiJSON);
       return;
     };
     fetchData();
-  }, []);
+  }, [sortField, sortOrder, filterField, filterValue, limit, page]);
 
   return (
     <div className="App">
       <Routes>
         <Route
           path="/blogs"
-          element={<BlogsPage blogs={serverJSON.message} />}
+          element={
+            <BlogsPage
+              blogs={serverJSON.message}
+              sortField={sortField}
+              setSortField={setSortField}
+              sortOrder={sortOrder}
+              setSortOrder={setSortOrder}
+              filterField={filterField}
+              setFilterField={setFilterField}
+              filterValue={filterValue}
+              setFilterValue={setFilterValue}
+              limit={limit}
+              setLimit={setLimit}
+              page={page}
+              setPage={setPage}
+            />
+          }
         ></Route>
       </Routes>
     </div>
